7c2ed18f7113865042aef6004c6b662617c36a06
Pruthuvi Fernando <pruthuvi.fernando@gmail.com>
2025-11-21 23:07:44 +0530

    complete up to 4.19

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,6 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (
+    authorization &&
+    authorization.startsWith('Bearer ')
+  ) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 // blogsRouter.get('', (request, response) => {
 //   Blog.find({}).then((blogs) => {
@@ -27,29 +39,42 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(
+    getTokenFrom(request),
+    process.env.SECRET
+  )
+  if (!decodedToken.id) {
+    return response
+      .status(401)
+      .json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!body.title || !body.url) {
     return response.status(400).end()
   }
 
-  const users = await User.find({})
+  // const users = await User.find({})
 
-  const selectedUser =
-    users[Math.floor(Math.random() * users.length)]
+  // const selectedUser =
+  //   users[Math.floor(Math.random() * users.length)]
 
   const blog = new Blog({
     title: body.title,
     url: body.url,
     author: body.author,
     likes: body.likes,
-    user: selectedUser._id,
+    // user: selectedUser._id,
+    user: user.id,
   })
 
   const savedBlog = await blog.save()
-  selectedUser.blogs = selectedUser.blogs.concat(
-    savedBlog._id
-  )
-  await selectedUser.save()
+  // selectedUser.blogs = selectedUser.blogs.concat(
+  //   savedBlog._id
+  // )
+  // await selectedUser.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
 
   response.status(201).json(savedBlog)
 })
